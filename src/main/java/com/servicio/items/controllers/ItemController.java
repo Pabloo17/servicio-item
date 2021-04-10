de576c6035da606fbda8f78ae4d60e8984b8c001
Pablo <Pablo@DESKTOP-NPPIB0P.home>
2021-04-10 18:57:49 +0200

    añadida gestion de errores con hystrix

--- src/main/java/com/servicio/items/controllers/ItemController.java
+++ src/main/java/com/servicio/items/controllers/ItemController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.servicio.items.models.Item;
+import com.servicio.items.models.Producto;
 import com.servicio.items.models.service.ItemService;
 
 @RestController
@@ -21,9 +23,23 @@ public class ItemController {
     return itemService.findAll();
   }
 
+  @HystrixCommand(fallbackMethod = "metodoAlternativo")
   @GetMapping("/ver/{id}/{cantidad}")
   public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 
     return itemService.findById(id, cantidad);
+  }
+
+  public Item metodoAlternativo(Long id, Integer cantidad) {
+    Item item = new Item();
+    Producto producto = new Producto();
+
+    item.setCantidad(cantidad);
+    producto.setId(id);
+    producto.setNombre("Camara Sony");
+    producto.setPrecio(500.00);
+    item.setProducto(producto);
+
+    return item;
   }
 }
