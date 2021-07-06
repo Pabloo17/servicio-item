779ad086da54a9f8aac128d5eb02119fe132f987
Pablo <pablochester92@gmail.com>
2021-07-06 21:50:55 +0200

    added endpoint to read properties config

--- src/main/java/com/servicio/items/controllers/ItemController.java
+++ src/main/java/com/servicio/items/controllers/ItemController.java
@@ -1,9 +1,13 @@
 package com.servicio.items.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +19,18 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.servicio.commons.models.entity.Producto;
 import com.servicio.items.models.Item;
 import com.servicio.items.models.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 public class ItemController {
   /** Qualifier es para indicar que implementacion usar */
   @Autowired
   @Qualifier("serviceFeign")
   private ItemService itemService;
+
+  @Value("${configuracion.texto}")
+  private String texto;
 
   @GetMapping("/listar")
   public List<Item> listar() {
@@ -64,5 +73,16 @@ public class ItemController {
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void eliminar(@RequestBody Producto producto, @PathVariable Long id) {
     itemService.delete(id);
+  }
+
+  @GetMapping("/obtener-config")
+  public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+
+    log.info(texto);
+    Map<String, String> json = new HashMap<>();
+    json.put("texto", texto);
+    json.put("puerto", puerto);
+
+    return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
   }
 }
